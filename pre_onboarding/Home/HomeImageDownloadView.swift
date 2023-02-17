a864b45611a9daf3e162784a2dde332a4659e196
hogumachu <castle_jun@daum.net>
2023-02-17 13:16:55 +0900

    feat: add home view

---
+++ pre_onboarding/Home/HomeImageDownloadView.swift
@@ -0,0 +1,90 @@
+//
+//  HomeImageDownloadView.swift
+//  pre_onboarding
+//
+//  Created by 홍성준 on 2023/02/17.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+protocol HomeImageDownloadViewDelegate: AnyObject {
+    
+    func homeImageDownloadViewDidTapLoadButton(_ view: HomeImageDownloadView)
+    
+}
+
+final class HomeImageDownloadView: UIView {
+    
+    weak var delegate: HomeImageDownloadViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupLayout()
+        self.setupAttributes()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateProgress(_ progress: Float) {
+        self.progressView.progress = progress
+    }
+    
+    func setImage(_ image: UIImage?) {
+        self.imageView.image = image
+    }
+    
+    func reset() {
+        self.progressView.progress = 0
+        self.imageView.image = UIImage(systemName: "photo")?.withRenderingMode(.alwaysTemplate)
+    }
+    
+    private func setupLayout() {
+        self.addSubview(self.imageView)
+        self.imageView.snp.makeConstraints { make in
+            make.top.leading.bottom.equalToSuperview()
+            make.size.equalTo(CGSize(width: 100, height: 60))
+        }
+        
+        
+        self.addSubview(self.loadButton)
+        self.loadButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.size.equalTo(CGSize(width: 60, height: 30))
+        }
+        
+        self.addSubview(self.progressView)
+        self.progressView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(self.imageView.snp.trailing).offset(5)
+            make.trailing.equalTo(self.loadButton.snp.leading)
+            make.height.equalTo(5)
+        }
+    }
+    
+    private func setupAttributes() {
+        self.imageView.do {
+            $0.image = UIImage(systemName: "photo")?.withRenderingMode(.alwaysTemplate)
+        }
+        
+        self.loadButton.do {
+            $0.backgroundColor = .systemBlue
+            $0.setTitle("Load", for: .normal)
+            $0.layer.cornerRadius = 8
+            $0.addTarget(self, action: #selector(loadButtonDidTap(_:)), for: .touchUpInside)
+        }
+    }
+    
+    @objc private func loadButtonDidTap(_ sender: UIButton) {
+        self.delegate?.homeImageDownloadViewDidTapLoadButton(self)
+    }
+    
+    private let imageView = UIImageView(frame: .zero)
+    private let progressView = UIProgressView(frame: .zero)
+    private let loadButton = UIButton(frame: .zero)
+    
+}
--- pre_onboarding/Home/HomeView.swift
+++ pre_onboarding/Home/HomeView.swift
@@ -5,4 +5,95 @@
 //  Created by 홍성준 on 2023/02/17.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+protocol HomeViewDelegate: AnyObject {
+    
+    func homeViewDidTapLoadImageButton(_ view: HomeView, at tag: Int)
+    func homeViewDidTapLoadAllImageButton(_ view: HomeView)
+    
+}
+
+final class HomeView: UIView {
+    
+    weak var delegate: HomeViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupLayout()
+        self.setupAttributes()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        self.addSubview(self.imageViewStackView)
+        self.imageViewStackView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+        }
+        
+        self.imageViewStackView
+            .subviews
+            .forEach { $0.removeFromSuperview() }
+        
+        self.imageViews = []
+        self.imageViews = (0..<self.imageCount)
+            .map { _ in HomeImageDownloadView(frame: .zero) }
+            .enumerated()
+            .map { offset, view in
+                view.do {
+                    $0.delegate = self
+                    $0.tag = offset
+                    self.imageViewStackView.addArrangedSubview($0)
+                }
+                return view
+            }
+        
+        self.addSubview(self.loadAllButton)
+        self.loadAllButton.snp.makeConstraints { make in
+            make.top.equalTo(self.imageViewStackView.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(30)
+        }
+    }
+    
+    private func setupAttributes() {
+        self.imageViewStackView.do {
+            $0.spacing = 10
+            $0.axis = .vertical
+            $0.distribution = .fillEqually
+            $0.alignment = .fill
+        }
+        
+        self.loadAllButton.do {
+            $0.backgroundColor = .systemBlue
+            $0.setTitle("Load All Images", for: .normal)
+            $0.layer.cornerRadius = 8
+            $0.addTarget(self, action: #selector(loadAllButtonDidTap(_:)), for: .touchUpInside)
+        }
+    }
+    
+    @objc private func loadAllButtonDidTap(_ sender: UIButton) {
+        self.delegate?.homeViewDidTapLoadAllImageButton(self)
+    }
+    
+    private let imageCount = 5
+    
+    private var imageViews: [HomeImageDownloadView] = []
+    private let imageViewStackView = UIStackView(frame: .zero)
+    private let loadAllButton = UIButton(frame: .zero)
+    
+}
+
+extension HomeView: HomeImageDownloadViewDelegate {
+    
+    func homeImageDownloadViewDidTapLoadButton(_ view: HomeImageDownloadView) {
+        let tag = view.tag
+        self.delegate?.homeViewDidTapLoadImageButton(self, at: tag)
+    }
+    
+}
